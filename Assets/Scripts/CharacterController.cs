ddccc19762ffb83e8f8a3e3c64a0e47ce00985d4
Berend Meijer <berend.sjaak@gmail.com>
2020-09-12 12:43:42 +0200

    Added simple jump on button button press input.

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D characterRigidBody = null;
+
+    [SerializeField]
+    private float jumpForce = 50.0f;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            characterRigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
