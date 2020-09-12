893e754c4d910465a5bfe9323dd26bd4219760a9
Berend Meijer <berend.sjaak@gmail.com>
2020-09-12 13:18:57 +0200

    Added a velocity constraint to the character to control it's maximum horizontal velocity.

---
+++ Assets/Scripts/Extensions/Vector2Extensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class Vector2Extensions
+{
+    public static Vector2 Clamp(this Vector2 value, Vector2 min, Vector2 max)
+    {
+        return new Vector2(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y));
+    }
+}
---
+++ Assets/Scripts/VelocityConstraint2D.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class VelocityConstraint2D : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rigidBody = null;
+
+    [SerializeField]
+    public Vector2 velocityLimit = Vector2.one;
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = rigidBody.velocity.Clamp(-velocityLimit, velocityLimit);
+    }
+
+    private void Reset()
+    {
+        rigidBody = this.GetComponent<Rigidbody2D>();
+    }
+}
