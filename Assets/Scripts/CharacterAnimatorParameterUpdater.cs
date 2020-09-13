44cc3587fa3c0f0238d610e9fabe92bd28a3da04
Berend Meijer <berend.sjaak@gmail.com>
2020-09-13 13:23:44 +0200

    Added IsGrounded boolean to the character animation controller so the character always transitions to the jump animation whenever it is in the air.

--- Assets/Scripts/CharacterAnimatorParameterUpdater.cs
+++ Assets/Scripts/CharacterAnimatorParameterUpdater.cs
@@ -6,11 +6,15 @@ public class CharacterAnimatorParameterUpdater : MonoBehaviour
     private Rigidbody2D rigidBody = null;
 
     [SerializeField]
+    private CharacterController characterController = null;
+
+    [SerializeField]
     private Animator animator = null;
 
     private void Update()
     {
         animator.SetFloat("Horizontal velocity", rigidBody.velocity.x);
         animator.SetFloat("Vertical velocity", rigidBody.velocity.y);
+        animator.SetBool("IsGrounded", characterController.IsGrounded());
     }
 }
