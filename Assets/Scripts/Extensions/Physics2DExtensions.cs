323ee94cfcdba735b9182299a3f7bb723c65079d
Berend Meijer <berend.sjaak@gmail.com>
2020-09-13 16:28:09 +0200

    Simplified return statement.

--- Assets/Scripts/Extensions/Physics2DExtensions.cs
+++ Assets/Scripts/Extensions/Physics2DExtensions.cs
@@ -10,13 +10,6 @@ public static class Physics2DExtensions
         RaycastHit2D raycastHit = Physics2D.BoxCast(origin, size, angle, direction, distance);
 
         // A 'collider' property of null indicates that no collider was hit in the boxcast.
-        if (raycastHit.collider == null)
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
+        return raycastHit.collider != null;
     }
 }
